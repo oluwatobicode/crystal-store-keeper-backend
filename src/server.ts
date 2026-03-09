223dbd3c5b64318b9a801ae4c983b1cfae8ec297
Coding Ninja <codingninja@example.com>
2026-03-09 22:16:55 +0100

    fix-cors-error

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,13 @@ connectDb();
 app.use(helmet());
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "https://crystal-stock-keeper.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
